e474d71dd049dbad64b0700258d9a06aa9bc1967
Igwe Augustine <augustusbest00@gmail.com>
2020-08-19 17:09:19 +0100

    added homepage

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../components/layout';
+
+const Home = () => {
+    return (
+        <Layout>
+            Home
+        </Layout>
+    )
+}
+
+export { Home }
