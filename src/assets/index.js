29192feaccfc82840c3a011301b2f1a1064aa641
Igwe Augustine <augustusbest00@gmail.com>
2020-08-19 22:56:41 +0100

    used for exporting assets

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import overview from './overview.svg';
+import wallet from './wallet.svg';
+import wallet2 from './wallet2.svg';
+import wallet3 from './wallet3.svg';
+import circle from './circle.svg';
+import order1 from './order1.svg';
+import order2 from './order2.svg';
+import order3 from './order3.svg';
+import user from './user.svg';
+import chart from './chart.svg';
+import big_graph from './big_graph.png'
+
+export {
+    overview,
+    wallet,
+    wallet2,
+    wallet3,
+    circle,
+    order1,
+    order2,
+    order3,
+    user,
+    chart,
+    big_graph
+}
