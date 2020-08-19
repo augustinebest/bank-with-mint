d9f0a208579e5858b300fc203985c7496da9e507
Igwe Augustine <augustusbest00@gmail.com>
2020-08-19 21:09:22 +0100

    added mobile views for navbar and sidebar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Navbar } from './navbar';
-import { Sidebar } from './sidebar';
+import { DesktopNavbar } from './navbar/desktopNavbar';
+import { DesktopSidebar } from './sidebar/desktopSidebar';
 import '../App.css'
+import { MobileNavbar } from './navbar/mobileNavbar';
+import { MobileSidebar } from './sidebar/mobileSidebar';
 
 const Layout = (props) => {
     return (
         <div>
-            <Navbar />
-            <Sidebar />
+            <div className='desktopNavbar'>
+                <DesktopNavbar />
+            </div>
+            <div className='mobileNavbar'>
+                <MobileNavbar />
+            </div>
+            <div className='desktopSidebar'>
+                <DesktopSidebar />
+            </div>
+            <div className='mobileSidebar'>
+                <MobileSidebar />
+            </div>
             <div className='main_dashboard'>
                 {props.children}
             </div>
