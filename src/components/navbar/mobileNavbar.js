c2ccb00c03ac83e6c1718d012607d802cdd799d0
Igwe Augustine <augustusbest00@gmail.com>
2020-08-19 22:57:00 +0100

    added mobile sidebar

---
+++ src/components/navbar/mobileNavbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MobileNavbar = () => {
+    return (
+        <div className='mobile_nav'>
+            MobileNavbar
+        </div>
+    )
+}
+
+export { MobileNavbar };
