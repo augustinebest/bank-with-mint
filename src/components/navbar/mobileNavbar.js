87e808e4315199d3188e8c938ec557d393d687ae
Igwe Augustine <augustusbest00@gmail.com>
2020-08-20 00:40:17 +0100

    mobile responsive navbar

--- src/components/navbar/mobileNavbar.js
+++ src/components/navbar/mobileNavbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const MobileNavbar = () => {
+const MobileNavbar = (props) => {
     return (
         <div className='mobile_nav'>
-            MobileNavbar
+            <div className='logo_'>TransMonitor</div>
+            <>
+                {
+                    !props.status ?
+                        <div onClick={props.click}><i className='fas fa-bars hamburger'></i></div>
+                        :
+                        <div onClick={props.click}><i className='fas fa-times hamburger'></i></div>
+                }
+            </>
         </div>
     )
 }
